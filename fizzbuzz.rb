c6eb0dbc987b1cd7c1f257d8e138fad39f7b2671
Paul Ashour <paul.ashour@gmail.com>
2018-10-01 03:02:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  case int
-  when % 15 == 0
+  case 
+  when int % 15 == 0
     'FizzBuzz'
-  elsif int % 3 == 0 
+  when int % 3 == 0 
     'Fizz'
-  elsif int % 5 == 0
+  when int % 5 == 0
     'Buzz'
   end
 end
